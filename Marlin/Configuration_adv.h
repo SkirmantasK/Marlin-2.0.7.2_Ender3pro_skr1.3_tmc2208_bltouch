b0ca1d0db6438b4472b948511a927a504e4c5efa
Skirmantas <admin@linuxsec.eu>
2021-03-22 20:03:02 +0200

    Just to be safe

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1082,7 +1082,7 @@
   #if HAS_BED_PROBE
     #define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
-      #define PROBE_OFFSET_START -4.0   // Estimated nozzle-to-probe Z offset, plus a little extra
+      #define PROBE_OFFSET_START -2.0   // Estimated nozzle-to-probe Z offset, plus a little extra
     #endif
   #endif
 
